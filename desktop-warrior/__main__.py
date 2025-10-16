4ef145d62f32336f2faa9427bdae2d6b2486e11a
Hendrik Makait <hendrik@makait.com>
2025-10-16 19:34:32 +0200

    Add ls tool

--- desktop-warrior/__main__.py
+++ desktop-warrior/__main__.py
@@ -4,4 +4,8 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
-    agent(client=OpenAI(), model="gpt-5", system="You are a helpful assistant")
+    agent(
+        client=OpenAI(),
+        model="gpt-5",
+        system="You are a helpful assistant with access to the file system",
+    )
--- desktop-warrior/agent.py
+++ desktop-warrior/agent.py
@@ -1,5 +1,47 @@
+import json
+import os
+from typing import Final
 from openai import OpenAI
-from openai.types.chat import ChatCompletionMessageParam
+from openai.types.chat import (
+    ChatCompletionFunctionToolParam,
+    ChatCompletionMessageFunctionToolCall,
+    ChatCompletionMessageParam,
+    ChatCompletionToolMessageParam,
+)
+
+TOOLS: Final[list[ChatCompletionFunctionToolParam]] = [
+    {
+        "type": "function",
+        "function": {
+            "name": "ls",
+            "description": "list directory contents",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "path": {
+                        "type": "string",
+                        "description": "The path to the directory, e.g., ~/Desktop",
+                    },
+                },
+                "required": ["path"],
+            },
+        },
+    },
+]
+
+
+def execute_tool(
+    call: ChatCompletionMessageFunctionToolCall,
+) -> ChatCompletionToolMessageParam:
+    func = call.function.name
+    args = json.loads(call.function.arguments)
+
+    if func == "ls":
+        output = os.listdir(args["path"])
+    else:
+        output = "Unknown tool"
+    return {"role": "tool", "tool_call_id": call.id, "content": json.dumps(output)}
+
 
 def agent(client: OpenAI, model: str, system: str) -> None:
     messages: list[ChatCompletionMessageParam] = [{"role": "system", "content": system}]
@@ -8,10 +50,22 @@ def agent(client: OpenAI, model: str, system: str) -> None:
         prompt = input("User: ")
         messages.append({"role": "user", "content": prompt})
 
-        completion = client.chat.completions.create(model=model, messages=messages)
+        agent_turn = True
+        while agent_turn:
+            completion = client.chat.completions.create(
+                model=model, messages=messages, tools=TOOLS
+            )
+            response = completion.choices[0].message
+            messages.append(response)
 
-        answer = None
-        if completion.choices:
-            answer = completion.choices[0].message.content
-        messages.append({"role": "assistant", "content": answer})
-        print(f"Assistant: {answer or ''}")
+            if response.tool_calls:
+                # When thinking out loud, print thoughts
+                if response.content:
+                    print(response.content)
+                for tool_call in response.tool_calls:
+                    messages.append(execute_tool(tool_call))
+                agent_turn = True
+            else:
+                # Regular response - print and exit inner loop
+                print(f"Assistant: {response.content or ''}")
+                agent_turn = False
